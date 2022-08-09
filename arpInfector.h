9fa7c68d4c4f99f015dc032b8ef3e6a9b0deff0c
OneDayGinger <hsremnant9@gmail.com>
2022-08-09 09:00:57 +0100

    Modified arpInfector, final version

--- arpInfector.h
+++ arpInfector.h
@@ -13,29 +13,17 @@ struct EthArpPacket {
     ArpHdr arp_;
 };
 
-struct Ip_Mac_map {
-    Mac eth_smac;
-    Mac eth_dmac;
-    Mac arp_smac;
-    Ip arp_sip;
-    Mac arp_tmac;
-    Ip arp_tip;
-};
-
 class ArpInfector {
 private:
     char* device_;
-
     Mac* my_MAC_;
     Mac* victim_MAC_;
-
     Ip* victim_ip_;
     Ip* target_ip_;
-
     pcap_t* handle_;
     char errbuf_[PCAP_ERRBUF_SIZE];
 
-    void send_arp_request(Ip_Mac_map* ipmac_map, int type);
+    void send_arp_request(EthArpPacket* packet);
     Mac* get_my_MAC();
     Mac* capture_arp_packet(const u_char* packet);
 
